bee2ff2bf2848bde82170cc742668a040dc9707a
Kiloutre <kilowwwww@gmail.com>
2021-05-28 21:09:35 +0200

    Remove misleading comment from build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,1 @@
-
-# pyinstaller --version
-# Above command should return 3.6
-
 pyinstaller --windowed --noconsole --clean --icon=InterfaceData/natsumi.ico --add-data InterfaceData;InterfaceData --add-data CameraAnimations;CameraAnimations --add-data game_addresses.txt;. --name TekkenMovesetExtractor GUI_TekkenMovesetExtractor.py
